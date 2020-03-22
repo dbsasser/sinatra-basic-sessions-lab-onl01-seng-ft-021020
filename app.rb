614280e567474e6bd355a0d23b343242516c7209
Daniel Sasser <danielbsasser@gmail.com>
2020-03-22 13:34:12 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,13 +6,13 @@ class App < Sinatra::Base
     enable :sessions
     set :session_secret, "secret"
   end
-  
+
   get '/' do
     erb :index
   end
-  
+
   post '/checkout' do
-  
+
   end
 
 end
